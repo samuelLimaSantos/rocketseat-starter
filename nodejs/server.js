f46d0c6ed28d440d787c657fb1d00686a9fe274c
Samuel Santos <samuellima280499@gmail.com>
2020-07-07 18:39:59 -0300

    Updarte Node Course

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,22 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+// Iniciando o app
 const app = express();
 
-app.use(express.json()); // Configurando o express para aceitar formato json
-
-app.get("/", (request, response) => {
-    
-    
-    return response.json({ message: "Hello Rocketseat"});
+// Configurando o app para mandar json
+app.use(express.json());
+app.use(cors());
+// Iniciando o DB
+mongoose.connect("mongodb://192.168.99.100:27017/nodeapi", {
+  useNewUrlParser: true,
 });
 
+requireDir("./src/models");
 
+// Rotas
+app.use("/api", require("./src/routes"));
 
-
-
-
-
-
-
-
+// Criação da porta
 app.listen(3001);
